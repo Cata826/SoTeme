954be815d037c82f89ab412c2e0421ee0a675f6f
cata <balintcatalin826@gmail.com>
2023-05-02 08:16:49 +0300

    partial sec2+sect3

--- a2/a2.c
+++ a2/a2.c
@@ -4,20 +4,36 @@
 #include <unistd.h>
 #include "a2_helper.h"
 #include <pthread.h>
-void *thread_function(void* arg)
+#include <semaphore.h>
+
+sem_t sem; 
+void *thread_function(void *arg)
 {
-    int thread_num = *((int*) arg);
+    int thread_num = *((int *)arg);
     // if(thread_num==1)
-    // { 
+    // {
     //     info(BEGIN, 2, 3);
     //     info(BEGIN, 2, thread_num);
     // }
     // else
     info(BEGIN, 2, thread_num);
-    
-    
-    
-    
+
+    return NULL;
+}
+void *thread_function6(void *arg)
+{
+    int thread_num = *((int *)arg);
+    // if(thread_num==1)
+    // {
+    //     info(BEGIN, 2, 3);
+    //     info(BEGIN, 2, thread_num);
+    // }
+    // else
+    sem_wait(&sem);
+    info(BEGIN, 6, thread_num);
+    info(END, 6, thread_num);
+      sem_post(&sem); 
+        
 
     return NULL;
 }
@@ -36,32 +52,37 @@ int main(void)
     else if (pid2 == 0)
     {
         info(BEGIN, 2, 0);
-      // create threads
+        
+        // create threads
         pthread_t threads[4];
-        int thread_args[4]={1,2,3,4};
+        int thread_args[4];
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 2; i < 4; i++)
         {
             thread_args[i] = i + 1;
             pthread_create(&threads[i], NULL, thread_function, &thread_args[i]);
         }
-        //info(BEGIN,2,1);
-        // wait for threads to finish
+        for (int i = 0; i<= 1; i++)
+        {
+            thread_args[i] = i + 1;
+            pthread_create(&threads[i], NULL, thread_function, &thread_args[i]);
+        }
+
+        // info(BEGIN,2,1);
+        //  wait for threads to finish
         for (int i = 0; i < 4; i++)
         {
             pthread_join(threads[i], NULL);
         }
-     
         // printf("[CHILD 2] My PID is %d. My parent's PID is %d.\n", getpid(), getppid());
-        info(END, 2, 1);
-        info(END, 2, 3);
-        info(END, 2, 2);
-        info(END, 2, 4);
-        
+        for (int i = 0; i < 4; i++)
+        {
+            info(END, 2, i + 1);
+        }
+
         info(END, 2, 0);
         // free(thread_args);
         return 0;
-       
     }
 
     pid3 = fork();
@@ -86,10 +107,9 @@ int main(void)
             info(END, 7, 0);
             return 0;
         }
-         waitpid(pid7, NULL, 0);
-         waitpid(pid3, NULL, 0);
+        waitpid(pid7, NULL, 0);
+        waitpid(pid3, NULL, 0);
         info(END, 3, 0);
-       
 
         return 0;
     }
@@ -118,10 +138,8 @@ int main(void)
         }
 
         waitpid(pid8, NULL, 0);
-         waitpid(pid4, NULL, 0);
+        waitpid(pid4, NULL, 0);
         info(END, 4, 0);
-
-    
 
         return 0;
     }
@@ -146,9 +164,25 @@ int main(void)
     }
     else if (pid6 == 0)
     {
+        sem_init(&sem, 0, 4);
         info(BEGIN, 6, 0);
+         pthread_t threads1[50];
+    int thread_args1[50];
+    for (int i = 0; i < 50; i++)
+    {
+        thread_args1[i] = i + 1;
+        pthread_create(&threads1[i], NULL, thread_function6, &thread_args1[i]);
+       
+    }
+
+    for (int i = 0; i < 50; i++)
+    {
+        pthread_join(threads1[i], NULL);
+    }
+      
         // printf("[CHILD 6] My PID is %d. My parent's PID is %d.\n", getpid(), getppid());
         info(END, 6, 0);
+        sem_destroy(&sem);
         return 0;
     }
 
